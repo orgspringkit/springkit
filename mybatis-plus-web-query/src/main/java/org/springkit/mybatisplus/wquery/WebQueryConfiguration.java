5b3f84d7b59be6d14527ab93fc6a231a18528ae0
springkit <orgspringkit@gmail.com>
2024-01-19 18:51:46 +0900

    add Apache License 2.0

--- mybatis-plus-web-query/src/main/java/org/springkit/mybatisplus/wquery/WebQueryConfiguration.java
+++ mybatis-plus-web-query/src/main/java/org/springkit/mybatisplus/wquery/WebQueryConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2024, springkit (orgspringkit@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springkit.mybatisplus.wquery;
 
 import java.sql.Connection;
@@ -10,7 +25,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
--- springkit-test/src/main/java/org/springkit/kits/test/config/KitsTestConfiguration.java
+++ springkit-test/src/main/java/org/springkit/kits/test/config/KitsTestConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2024, springkit (orgspringkit@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springkit.kits.test.config;
 
 import java.sql.Connection;
@@ -6,12 +21,8 @@ import java.sql.SQLException;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
